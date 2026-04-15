4bc59be74fea1cfd7199231ed9bace9478035cd8
Falon Scheers <falon.scheers@humanrights.ca>
2026-04-15 16:26:28 -0600

    clooose 605 also not right, too high

--- analyze.js
+++ analyze.js
@@ -9,11 +9,12 @@ function report_unsafe(report, why){
 
 function is_report_safe(report, removed=null, original=null){
     let diff_fail = "do not differ by at least one and at most three";
-    let levels = report.split(" ");
+    let levelstrings = report.split(" ");
+    let levels = levelstrings.map(Number);
     //if (removed) console.log("trying ", levels, " with removed ", removed);
     for (const [pos, cur_level] of levels.entries()) {
         if (pos==0) continue;
-        let last_diff = (cur_level - levels[pos-1]);
+        let last_diff = cur_level - levels[pos-1];
         if (Math.abs(last_diff)<1 || Math.abs(last_diff)>3){
             if (removed=== null) {
                 removed = levels[pos-1];
@@ -28,7 +29,7 @@ function is_report_safe(report, removed=null, original=null){
         if (pos>=levels.length-1) {
             //at end make the classification
             if (original=== null) original = report;
-            console.log(`${original} is Safe with ${removed} removed`);
+            //console.log(`${original} is Safe with ${removed} removed`);
             return true;
         }
         else{
@@ -46,13 +47,43 @@ function is_report_safe(report, removed=null, original=null){
             }
             if ((last_diff<=0 && next_diff>=0) || (last_diff>=0 && next_diff<=0)){
                 if (removed=== null) {
-                    let direction = pos < (levels.length - 1) / 2 ? -1 : 1;
-                    let to_rem = (levels[pos-1]==levels[pos+1]) ? pos+(1*direction) : pos;
+                    let direction = pos < (levels.length - 1) / 2 ? -1 : 1; //first or 2nd half
+                    let to_rem = pos;
+                    //console.log("workin on, ", levels, "to_rem", to_rem);
+                    if (direction<0){ //look ahead if safe
+                        //console.log("look ahead, ","last diff: ",last_diff, "next_diff: ", next_diff, "cur_pos: ", pos);
+                        if (pos+2 < levels.length) {
+                            if ((levels[pos+2] < levels[pos+1]) && (last_diff>0) ){ //general dec
+                                //console.log("here1?", levels[pos+1] > levels[pos+2], levels[pos+2], levels[pos+1]);
+                                if (levels[pos-1] <= levels[pos+1]) to_rem=pos-1;
+                            }
+                            else if ((levels[pos+2] > levels[pos+1]) && last_diff<0){ //general inc
+                                //console.log("here2?");
+                                if (levels[pos-1] >= levels[pos+1]) to_rem=pos-1;
+                            }
+                        }
+                    }
+                    else{ //look back if safe
+                        //console.log("look back, ","last diff: ",last_diff, "next_diff: ", next_diff, "cur_pos: ", pos);
+                        if (pos-2 >= 0) {
+                            if((levels[pos-2] < levels[pos-1]) && next_diff<0){ //gen incr
+                                if (levels[pos+1] <= levels[pos-1]) to_rem=pos+1;
+                            }
+                            else if ((levels[pos-2] > levels[pos-1]) && next_diff>0){ //gen dec
+                                if (levels[pos+1] >= levels[pos-1]) to_rem=pos+1;
+                            }
+                        }
+                    }
+                    //console.log("to_rem", to_rem);
+                    //let to_rem = (levels[pos-1]==levels[pos+1]) ? pos+(1*direction) : pos;
                     removed = levels[to_rem];
                     levels.splice(to_rem, 1);
-                    return is_report_safe(levels.join(" "), removed, report);
+                    levels = levels.join(" ");
+                    //console.log("should try now, ", levels)
+                    return is_report_safe(levels, removed, report);
                 }
-                else {
+                else { //put old one back take next
+                    //levels.splice(pos+1, 1, removed);
                     report_unsafe(original, `all levels are not either all increasing or all decreasing`);
                     return false;
                 }
